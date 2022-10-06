9b4bb212f9ef127532f7245fb9183aad048bf328
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2022-10-06 16:51:15 +0200

    fix find_layer()
    next index has to be numeric, or it cant be used as the index, added comments as well

--- R/simplify_idaifield_resources.R
+++ R/simplify_idaifield_resources.R
@@ -25,10 +25,13 @@ find_layer <- function(resource = resource,
     return(NA)
   }
 
+  # get identifier, index and type of the parent resource
   liesWithin_index <- which(resource$relation.liesWithin == uidlist$identifier)
   liesWithin_identifier <- uidlist$identifier[liesWithin_index]
   liesWithin_type <- uidlist$type[liesWithin_index]
 
+  # if there is no df names "liesWithin" yet (i.e. liesWithin = NULL), create
+  # one with the data gathered above
   if (!is.data.frame(liesWithin)) {
     liesWithin <- data.frame(liesWithin_index = liesWithin_index,
                              liesWithin_identifier = liesWithin_identifier,
@@ -44,24 +47,34 @@ find_layer <- function(resource = resource,
     layer_type_list <- getOption("idaifield_types")$layers_strict
   }
 
+  # is any of the parent resources in the liesWithin-df a Layer?
   is_context <- liesWithin$liesWithin_type %in% layer_type_list
   if (any(is_context)) {
+    # return the layer/context if there is one
     in_layer <- which(is_context)
     in_layer <- liesWithin$liesWithin_identifier[in_layer]
     return(in_layer)
   } else {
+    # get the next row of the df to find its parent resources
     current <- nrow(liesWithin)
-    current <- liesWithin$liesWithin_index[current]
+    # will not work with chr value as it has to be the index
+    current <- as.numeric(liesWithin$liesWithin_index[current])
+    # get the identifier of the next parent resource
     next_liesWithin_identifier <- uidlist$liesWithin[current]
 
     if (is.na(next_liesWithin_identifier) || length(current) == 0) {
+      # this happens if there is no parent resource or no df
       return(NA)
     } else {
+      # get the index and type of the parent
       next_liesWithin_index <- which(uidlist$identifier == next_liesWithin_identifier)
       next_liesWithin_type <- uidlist$type[next_liesWithin_index]
+      # add as a row to the data.frame
       liesWithin <- rbind(liesWithin, c(next_liesWithin_index,
                                         next_liesWithin_identifier,
                                         next_liesWithin_type))
+      # recursively call the function again to find the next parent, this
+      # time with the data.frame already existing
       find_layer(resource = resource,
                  uidlist = uidlist,
                  liesWithin = liesWithin)
