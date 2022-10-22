d2957339a327c967b44f53ee413ca0c1cd18c45b
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2022-10-22 11:46:55 +0200

    Update test-check_if_uid.R

--- tests/testthat/test-check_if_uid.R
+++ tests/testthat/test-check_if_uid.R
@@ -7,6 +7,16 @@ test_that("check_if_uid refuses non-UID", {
   expect_false(check_if_uid(string = "börek"))
 })
 
-test_that("check_if_uid warns for vector", {
-  expect_warning(check_if_uid(string = c(1, 2)))
+test_that("check_if_uid identifies UIDs in vector", {
+  expect_true(check_if_uid(string = c("0324141a-8201-c5dc-631b-4dded4552ac4",
+                                      "185d08b6-b9a5-7464-9e44-ecf2fe29bc36",
+                                      2))[1])
+  expect_true(check_if_uid(string = c("0324141a-8201-c5dc-631b-4dded4552ac4",
+                                      "185d08b6-b9a5-7464-9e44-ecf2fe29bc36",
+                                      2))[2])
+  expect_false(check_if_uid(string = c("0324141a-8201-c5dc-631b-4dded4552ac4",
+                                       "185d08b6-b9a5-7464-9e44-ecf2fe29bc36",
+                                       2))[3])
 })
+
+
