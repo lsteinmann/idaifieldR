aa88c36e27c43e165f770ec1f40b411ab2e8bc6d
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2022-10-23 15:02:12 +0200

    add check and unnest as export

---
+++ R/check_and_unnest.R
@@ -0,0 +1,39 @@
+#' Check and unnest a list
+#'
+#' Checks for list of class "idaifield_docs" and if the object is already
+#' unnested (i.e. of class "idaifield_resources"); if it is not, does so.
+#' If it cannot be processed, because it is not an idaifield_docs or
+#' idaifield_resources object, throws error.
+#' TODO: wrap in trycatch to process anyway if possible
+#'
+#' @param idaifield_docs An object to be used by one of the
+#' functions in this package
+#'
+#' @return if already unnested, the same object as handed to it. if not,
+#' the same list with the toplevel removed.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' idaifield_docs <- get_idaifield_docs(projectname = "rtest",
+#' connection = connect_idaifield(serverip = "127.0.0.1",
+#' user = "R",
+#' pwd = "password"))
+#'
+#' check_and_unnest(idaifield_docs)
+#' }
+check_and_unnest <- function(idaifield_docs) {
+  check <- check_if_idaifield(idaifield_docs)
+  if (check["idaifield_docs"]) {
+    idaifield_docs <- unnest_docs(idaifield_docs)
+    return(idaifield_docs)
+  } else if (check["idaifield_resources"]) {
+    return(idaifield_docs)
+  } else if (check["idaifield_simple"]) {
+    return(idaifield_docs)
+  } else {
+    warning("Not processed, did nothing.")
+    return(idaifield_docs)
+  }
+}
--- R/utilities.R
+++ R/utilities.R
@@ -88,49 +88,6 @@ check_if_idaifield <- function(testobject) {
   # However, I don't really like it and am thinking about changing it again.
 }
 
-
-
-#' Check and unnest a list
-#'
-#' Checks for list of class "idaifield_docs" and if the object is already
-#' unnested (i.e. of class "idaifield_resources"); if it is not, does so.
-#' If it cannot be processed, because it is not an idaifield_docs or
-#' idaifield_resources object, throws error.
-#' TODO: wrap in trycatch to process anyway if possible
-#'
-#' @param idaifield_docs An object to be used by one of the
-#' functions in this package
-#'
-#' @return if already unnested, the same object as handed to it. if not,
-#' the same list with the toplevel removed.
-#'
-#' @keywords internal
-#'
-#' @examples
-#' \dontrun{
-#' idaifield_docs <- get_idaifield_docs(projectname = "rtest",
-#' connection = connect_idaifield(serverip = "127.0.0.1",
-#' user = "R",
-#' pwd = "password"))
-#'
-#' check_and_unnest(idaifield_docs)
-#' }
-check_and_unnest <- function(idaifield_docs) {
-  check <- check_if_idaifield(idaifield_docs)
-  if (check["idaifield_docs"]) {
-    idaifield_docs <- unnest_docs(idaifield_docs)
-    return(idaifield_docs)
-  } else if (check["idaifield_resources"]) {
-    return(idaifield_docs)
-  } else if (check["idaifield_simple"]) {
-    return(idaifield_docs)
-  } else {
-    warning("Not processed, did nothing.")
-    return(idaifield_docs)
-  }
-}
-
-
 #' check_for_sublist
 #'
 #' Checks if a list has sublists and returns TRUE if so
