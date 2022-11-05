25ca2ea0f2f083cc8e1329d6c8ee65c50f4c47d1
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2022-11-05 16:43:23 +0100

    Update test-simplify_idaifield_resources.R

--- tests/testthat/test-simplify_idaifield_resources.R
+++ tests/testthat/test-simplify_idaifield_resources.R
@@ -2,11 +2,51 @@ source(file = "../load_testdata.R")
 
 uidlist <- get_uid_list(test_docs)
 
+item <- which(uidlist$identifier == "Keramik mit Multipolygon")
 
+test_that("geometry is gone", {
+  test <- simplify_single_resource(test_resources[[item]],
+                                   uidlist = uidlist,
+                                   keep_geometry = FALSE)
+  expect_null(test$geometry)
+})
+
+test_that("geometry is matrix", {
+  test <- simplify_single_resource(test_resources[[item]],
+                                   uidlist = uidlist,
+                                   keep_geometry = TRUE)
+  test <- test$geometry
+  expect_true("matrix" %in% class(test$coordinates[[1]]))
+})
+
+item <- match("Testformular", uidlist$type)
+
+test_that("removes configname from type", {
+  test <- simplify_single_resource(test_resources[[item]],
+                                   uidlist = uidlist,
+                                   keep_geometry = TRUE)
+  expect_false(grepl(":", test$type))
+})
+
+test_that("removes configname from field", {
+  test <- simplify_single_resource(test_resources[[item]],
+                                   uidlist = uidlist,
+                                   keep_geometry = TRUE)
+  expect_false(any(grepl(":", names(test))))
+})
+
+
+
+test_simple <- simplify_idaifield(test_resources,
+                                  keep_geometry = TRUE,
+                                  replace_uids = TRUE)
+
+mat <- idaifield_as_matrix(test_simple)
+colnames(mat)
 
 items <- sample(seq_along(test_resources), size = 5)
 for (item in items) {
-  print(paste("--------------------------###### Nr.: ", item))
+#  print(paste("--------------------------###### Nr.: ", item))
 
   test_that("error when no identifier", {
     test_resources[[item]]$identifier <- NULL
@@ -26,23 +66,6 @@ for (item in items) {
                                      uidlist = NULL)
     expect_identical(class(test), "list")
   })
-
-  if (!is.null(test_resources[[item]]$geometry)) {
-    test_that("geomtry is gone", {
-      test <- simplify_single_resource(test_resources[[item]],
-                                       uidlist = uidlist,
-                                       keep_geometry = FALSE)
-      expect_null(test$geometry)
-    })
-
-    test_that("geometry is matrix", {
-      test <- simplify_single_resource(test_resources[[item]],
-                                       uidlist = uidlist,
-                                       keep_geometry = TRUE)
-      test <- test$geometry
-      expect_true("matrix" %in% class(test$coordinates[[1]]))
-    })
-  }
 
   if (!is.null(test_resources[[item]]$dimensionLength)) {
     test_that("dimension is replaced", {
