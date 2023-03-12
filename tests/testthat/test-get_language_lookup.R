935292d6f8bba6eefa8677f3a468a2f2b6c7f34b
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-03-12 18:53:35 +0300

    test language lookup creation

--- tests/testthat/test-get_language_lookup.R
+++ tests/testthat/test-get_language_lookup.R
@@ -1,6 +1,31 @@
 
 
 
+emptylist <- config$languages
+emptylist$en <- list()
+
+test_that("throws error for empty language list", {
+  expect_error(prep_language_list(emptylist, language = "en"),
+               "empty")
+})
+
+langlist <- config$languages
+names(langlist) <- c("de", "englisch", "ar")
+
+test_that("throws error for wrong names", {
+  expect_error(prep_language_list(langlist, language = "en"),
+               "format")
+})
+
+test_that("returns a df with var and label", {
+  lookup <- prep_language_list(config$languages, language = "en")
+  expect_equal(colnames(lookup), c("var", "label"))
+})
+
+
+
+
+
 skip_on_cran()
 
 connection <- connect_idaifield(serverip = "127.0.0.1",
@@ -14,24 +39,4 @@ error = function(cond) {
 })
 
 config <- get_configuration(connection, projectname = "milet")
-
-emptylist <- config$languages
-emptylist$en <- list()
-
-test_that("throws error for empty language list", {
-  expect_error(prep_language_list(emptylist, language = "en"),
-               "empty")
-})
-
-names(emptylist) <- c("de", "englisch", "fr")
-
-test_that("throws error for wrong names", {
-  expect_error(prep_language_list(emptylist, language = "en"),
-               "format")
-})
-
-test_that("returns a df with var and label", {
-  lookup <- prep_language_list(config$languages, language = "en")
-  expect_equal(colnames(lookup), c("var", "label"))
-})
 
