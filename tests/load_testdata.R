a899ffb85a591f34f189bd75d47b549b82a3a9a3
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-03-12 18:41:51 +0300

    update test data

--- tests/load_testdata.R
+++ tests/load_testdata.R
@@ -2,3 +2,6 @@ library(idaifieldR)
 test_docs <- readRDS(system.file("testdata", "idaifield_test_docs.RDS",
                                  package = "idaifieldR"))
 test_resources <- unnest_docs(test_docs)
+
+config <- readRDS(system.file("testdata", "rtest_config.RDS",
+                                   package = "idaifieldR"))
