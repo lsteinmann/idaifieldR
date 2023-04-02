017bfcb48cadbc7bdccddf12175e2594c30fe135
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-04-02 15:51:33 +0200

    data to import to couchdb

--- inst/testdata/make-testjson.R
+++ inst/testdata/make-testjson.R
@@ -1,19 +1,71 @@
-connection <- connect_idaifield(pwd ="hallo", project = "rtest")
-client <- proj_idf_client(connection, include = "all")
+library(jsonlite)
 
-import <- client$get(query = list(include_docs = "true"))
-import <- response_to_list(import)
+jsonl <- readLines(system.file("testdata", "rtest.jsonl",
+                               package = "idaifieldR"))
+jsonl <- lapply(jsonl, function(x) fromJSON(x, FALSE))
 
-import <- import$rows
 
-import <- lapply(import, function(x) {
-  x$doc$created <- NULL
-  x$doc$modified <- NULL
-  x$doc$`_rev` <- NULL
-  x$value <- NULL
-  x$doc$`_id` <- NULL
-  return(x$doc)
+
+
+list <- lapply(jsonl, function(x) {
+  if ("docs" %in% names(x)) {
+    x <- x$docs
+  } else {
+    x <- NULL
+  }
+})
+ind <- unlist(lapply(list, is.null))
+list <- list[!ind]
+
+docs <- do.call(append, list)
+
+import <- lapply(docs, function(x) {
+  x$created <- NULL
+  x$modified <- NULL
+  x$`_id` <- NULL
+  x$`_rev` <- NULL
+  x$`_revisions` <- NULL
+  return(x)
 })
 import <- list(docs = import)
+write(jsonlite::toJSON(import,
+                       simplifyVector = TRUE,
+                       auto_unbox = TRUE,
+                       pretty = TRUE), file = "inst/testdata/import.json")
 
-write(jsonlite::toJSON(import, simplifyVector = TRUE, pretty = TRUE), file = "inst/testdata/testtttt.json")
+
+
+
+
+
+
+
+
+unbox_resource <- function(resource) {
+  resource <- lapply(resource, function(x)
+    if (length(x) == 1) {
+      x <- unlist(x)
+    })
+}
+import_unboxed <- lapply(import, function(x) {
+  x$resource <- unbox_resource(x$resource)
+  return(x)
+})
+
+
+resource <- import[[1]]$resource
+length(resource[[2]])
+
+unlist(resource[2])
+jsonlite::unbox(unlist(resource[[2]]))
+
+
+
+?unbox
+
+
+jsonlite::unbox(import$docs[[1]]$resource$identifier)
+
+
+str(import$docs[[1]])
+
