d4acdde751cc70b8b7f59d49d479c949e6833984
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-04-08 11:18:56 +0200

    rename and update select-functions (idf_select_by and idf_show_categories)

--- R/select_idaifield.R
+++ R/select_idaifield.R
@@ -1,13 +1,14 @@
-#' show_categories
+#' @title Show the categories present in an `idaifield_docs`- or
+#' `idaifield_resources`-list
 #'
-#' Returns a list of categories present in the iDAI.field 2 / Field Desktop database.
+#' @description Returns a list of all categories present in the
+#' iDAI.field 2 / Field Desktop database the list was imported from.
 #'
-#' @param idaifield_docs An object as returned by `get_idaifield_docs(...)`;
-#' unnests to resource level if it didn't already happen.
+#' @param idaifield_docs An an `idaifield_docs`- or `idaifield_resources`-list
+#' as returned by `get_idaifield_docs(...)`.
 #'
-#' @return a character vector with the types represented in
-#' the idaifield_docs/resource
-#' @export
+#' @return A character vector with the unique categories present in
+#' the iDAI.field 2 / Field Desktop database the list was imported from.
 #'
 #' @examples
 #' \dontrun{
@@ -15,34 +16,49 @@
 #'                                 user = "R",
 #'                                 pwd = "hallo",
 #'                                 project = "rtest")
+#'
 #' idaifield_docs <- get_idaifield_docs(connection = connection)
 #'
-#' show_categories(idaifield_docs)
+#' idf_show_categories(idaifield_docs)
 #' }
-show_categories <- function(idaifield_docs) {
-
-  idaifield_docs <- check_and_unnest(idaifield_docs)
-
-  uid_category_list <- get_uid_list(idaifield_docs)
-  db_categories <- unique(uid_category_list$category)
-  return(db_categories)
+#'
+#' @export
+idf_show_categories <- function(idaifield_docs) {
+  resources <- check_and_unnest(idaifield_docs)
+  cats <- lapply(resources, function(x) x$category)
+  cats <- unlist(cats, use.names = FALSE)
+  cats <- unique(cats)
+  return(cats)
 }
 
 
-#' select_by
+#' @title Deprecated function: Select/filter an `idaifield_resources`- or
+#' `idaifield_docs`-list
 #'
-#' returns a subset of the docs list selected by category or isRecordedIn
+#' @description This function has been deprecated in favor
+#' of \code{\link{idf_select_by}}. It returns a subset of the docs list
+#' selected by category or isRecordedIn
 #'
-#' @param idaifield_docs An object as returned by get_idaifield_docs(...)
+#' @details Subset or filter the list of the docs or resources by
+#' category or isRecordedIn. You may want to consider querying the database
+#' directly using \code{\link{idf_query}} or \code{\link{idf_index_query}}.
+#'
+#' @param idaifield_docs An `idaifield_resources`- or `idaifield_docs`-list
+#' as returned by `get_idaifield_docs()` etc.
 #' @param by must be either category (to select by resource category) or
 #' isRecordedIn (to select by container-resource (Survey-Area, Trench))
-#' @param value Character expected, should be the internal Name of the
-#' that will be selected for (e.g. "Layer", "Pottery"), can be vector of
-#' multiple Categories / Operations
+#' @param value character. Should be the internal name of the field
+#' that will be selected for (e.g. "Layer", "Pottery"), can also be vector of
+#' multiple Categories or Operations
 #'
-#' @return a list of class idaifield_resources containing the resources
-#' which are of the selected category
+#' @return A list of class `idaifield_resources` containing the resources
+#' which are of the selected category or in the selected operation.
+#'
 #' @export
+#'
+#' @keywords deprecated
+#'
+#' @seealso \code{\link{idf_select_by}}
 #'
 #' @examples
 #' \dontrun{
@@ -58,6 +74,49 @@ select_by <- function(idaifield_docs,
                       by = c("category", "isRecordedIn"),
                       value = NULL) {
 
+  .Deprecated("idf_select_by()", package = "idaifieldR", old = "select_by")
+  result <- idf_select_by(idaifield_docs,
+                          by = by,
+                          value = value)
+  return(result)
+}
+
+#' @title Select/filter an `idaifield_resources`- or `idaifield_docs`-list
+#'
+#' @description Subset or filter the list of the docs or resources by
+#' category or isRecordedIn. You may want to consider querying the database
+#' directly using \code{\link{idf_query}} or \code{\link{idf_index_query}}.
+#'
+#' @param idaifield_docs An `idaifield_resources`- or `idaifield_docs`-list
+#' as returned by `get_idaifield_docs()` etc.
+#' @param by must be either category (to select by resource category) or
+#' isRecordedIn (to select by container-resource (Survey-Area, Trench))
+#' @param value character. Should be the internal name of the field
+#' that will be selected for (e.g. "Layer", "Pottery"), can also be vector of
+#' multiple Categories or Operations
+#'
+#' @return A list of class `idaifield_resources` containing the resources
+#' which are of the selected category or in the selected operation.
+#'
+#' @export
+#'
+#' @seealso \code{\link{get_idaifield_docs}},
+#' \code{\link{idf_query}}, \code{\link{idf_index_query}}
+#'
+#' @examples
+#' \dontrun{
+#' connection <- connect_idaifield(serverip = "127.0.0.1",
+#' user = "R", pwd = "hallo", project = "rtest")
+#' idaifield_docs <- get_idaifield_docs(connection = connection)
+#'
+#' idaifield_layers <- select_by(idaifield_docs,
+#' by = "category",
+#' value = "Layer")
+#' }
+idf_select_by <- function(idaifield_docs,
+                          by = c("category", "isRecordedIn"),
+                          value = NULL) {
+
   if (length(by) > 1) {
     message("Please select only one of 'category' or 'isRecordedIn' (using first)")
     by <- by[1]
@@ -66,20 +125,13 @@ select_by <- function(idaifield_docs,
     stop("No value given for selection.")
   }
 
-  idaifield_docs <- check_and_unnest(idaifield_docs)
-  uidlist <- get_uid_list(idaifield_docs)
-
-  col <- colnames(uidlist) == by
-
-  ind <- which(uidlist[, col] %in% value)
-
-  selected_docs <- idaifield_docs[ind]
-  selected_docs <- structure(selected_docs, class = "idaifield_resources")
-
+  resources <- check_and_unnest(idaifield_docs)
+  result <- Filter(function(x) x[[by]] == value, resources)
 
   attributes <- attributes(idaifield_docs)
-  attributes(selected_docs) <- attributes[-which(names(attributes) == "names")]
-  names(selected_docs) <- names(idaifield_docs[ind])
+  attributes <- attributes[-which(names(attributes) == "names")]
+  attributes(result) <- append(attributes(result), attributes)
+  result <- structure(result, class = "idaifield_resources")
 
-  return(selected_docs)
+  return(result)
 }
