0fd11657ff8dc5d51e666d4d612f7655906157ac
Lisa Steinmann <lisa.steinmann@dainst.de>
2026-03-13 17:49:14 +0100

    stop instead of message

--- R/deprecated.R
+++ R/deprecated.R
@@ -1,12 +1,16 @@
 check_if_idaifield <- function(...) {
-  message("`check_if_idaifield()` has been removed.")
+  stop("`check_if_idaifield()` has been removed.")
 }
 
 reformat_geometry <- function(...) {
-  message("`reformat_geometry()` has been removed.")
+  stop("`reformat_geometry()` has been removed.")
 }
 
 download_language_list <- function(...) {
-  message("`download_language_list()` has been removed.")
+  stop("`download_language_list()` has been removed.")
+}
+
+get_field_inputtypes <- function(...) {
+  stop("`get_field_inputtypes()` has been removed.")
 }
 
