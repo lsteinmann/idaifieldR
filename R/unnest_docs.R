05a3abb9ef7110997ce63bd8495da881d6adf2d2
Lisa Steinmann <lisa.steinmann@rub.de>
2026-03-14 12:08:26 +0100

    use find_named_list() utility

--- R/unnest_docs.R
+++ R/unnest_docs.R
@@ -43,7 +43,6 @@ maybe_unnest_docs <- function(x) {
   return(x)
 }
 
-
 #' Unnesting a `idaifield_docs`-List down to resource level
 #'
 #' This function unnests the lists provided by iDAI.field / Field Desktop.
@@ -76,59 +75,12 @@ maybe_unnest_docs <- function(x) {
 #' idaifield_resources <- unnest_docs(idaifield_docs)
 #' }
 unnest_docs <- function(docs) {
-  resources <- find_resource(docs)
+  resources <- lapply(docs, function(doc) {
+    find_named_list("resource", doc)
+  })
   resources <- structure(resources, class = "idaifield_resources")
   attr(resources, "connection") <- attr(docs, "connection")
   attr(resources, "projectname") <- attr(docs, "projectname")
   attr(resources, "config") <- attr(docs, "config")
   return(resources)
-}
-
-#' Finding the "resource"-list in a CouchDB-output
-#'
-#' @param list a list formatted from a CouchDB-JSON-output
-#'
-#' @returns the list of resource-lists
-#'
-#' @keywords internal
-#'
-#' @examples
-#' \dontrun{
-#' testlist <- list(docs = list(list(resource = list(test = "test",
-#'                                                   test2 = "test2")),
-#'                              list(resource = list(test = "test",
-#'                                                   test2 = "test2"))),
-#'                  warning = "warning")
-#' find_resource(testlist)
-#' }
-find_resource <- function(list) {
-  has_resource <- lapply(list, function(x) "resource" %in% names(x))
-  has_resource <- unlist(has_resource)
-  has_resource <- any(has_resource)
-  if (has_resource) {
-    resource_list <- lapply(list, function(x) na_if_empty(x$resource))
-    return(resource_list)
-  } else {
-    has_docs <- "docs" %in% names(list)
-    has_rows <- "rows" %in% names(list)
-    if (has_docs) {
-      list <- list$docs
-      find_resource(list)
-    } else if (has_rows) {
-      list <- list$rows
-      find_resource(list)
-    } else {
-      has_doc <- lapply(list, function(x) "doc" %in% names(x))
-      has_doc <- unlist(has_doc)
-      has_doc <- all(has_doc)
-      if (has_doc) {
-        list <- lapply(list, function(x) x$doc)
-        find_resource(list)
-      } else {
-        # This is were the function should give up
-        warning("No resource-lists found.")
-        return(list)
-      }
-    }
-  }
 }
